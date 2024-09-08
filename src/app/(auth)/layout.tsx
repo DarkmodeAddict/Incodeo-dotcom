929eaaa6019f58c4d361830a0f0c8469abb0a151
Anurag <hamsen3880@gmail.com>
2024-09-08 21:31:54 +0530

    Change of Login and Register V0.9

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,28 +1,30 @@
 'use client'
 
-import { useAuthStore } from "@/store/Auth"
-import { useRouter } from "next/router"
-import React from "react"
+import { BackgroundBeams } from '@/components/ui/background-beams'
+import { useAuthStore } from '@/store/Auth'
+import { useRouter } from 'next/router'
+import React from 'react'
 
-const Layout = ({children}: {children: React.ReactNode}) => {
-    const {session} = useAuthStore()
-    const router = useRouter()
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  const { session } = useAuthStore()
+  const router = useRouter()
 
-    React.useEffect(() => {
-        if (session) {
-            router.push('/')
-        }
-    }, [session, router])
-
+  React.useEffect(() => {
     if (session) {
-        return null;
+      router.push('/')
     }
+  }, [session, router])
 
-    return (
-        <div className="">
-            <div className=""> {children} </div>
-        </div>
-    )
+  if (session) {
+    return null;
+  }
+
+  return (
+    <div className='relative flex min-h-screen flex-col items-center justify-center py-12'>
+      <BackgroundBeams />
+      <div className='relative'> {children} </div>
+    </div >
+  )
 }
 
 export default Layout
